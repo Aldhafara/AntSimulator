806a941dc21372446efc10355fe93558ac71e4c4
Marek Nawrocki <aldhafara@onet.pl>
2025-02-19 18:40:51 +0100

    Add gradle.properties

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,19 @@
+val localProperties = file("gradle-local.properties")
+if (localProperties.exists()) {
+    println("Loading local Gradle properties from gradle-local.properties")
+    localProperties.inputStream().use { stream ->
+        val properties = java.util.Properties().apply { load(stream) }
+        properties.forEach { key, value ->
+            val keyStr = key.toString()
+            val valueStr = value.toString()
+            if (!settings.extra.has(keyStr)) {
+                settings.extra[keyStr] = valueStr
+                println("Loaded local property: $keyStr")
+            }
+        }
+    }
+}
+
 rootProject.name = "AntSimulator"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
